a55d3ae4e051da4f4ce413bfbf0a10f867177ba1
heclair <heclair@hotmail.com>
2023-08-11 22:35:17 -0300

    aula-02

---
+++ aula02.sql
@@ -0,0 +1,7 @@
+create database bd_sistema_bancario;
+\c bd_sistema_bancario;
+create table tabela01(codigo integer);
+create table tabela02(conta integer);
+create table tabela03(agencia integer);
+create table tabela04(nome char);
+create table tabela05(valor float);
