b93d5874fcf6fde5b85987001ae927ee03900471
heclair <heclair@hotmail.com>
2023-08-18 22:05:53 -0300

    aula03

--- aula03.sql
+++ aula03.sql
@@ -2,6 +2,11 @@ create database bd_aula03;
 \c bd_aula03;
 
 create table tbl_cliente(codigo_cliente integer PRIMARY KEY, nome text NOT NULL, cidade text, endereco text);
-create table tbl_titulo(codigo_titulo integer PRIMARY KEY, titulo text NOT NULL, categoria);
+
+CREATE DOMAIN chk_categoria text CHECK(VALUE= 'DRAMA' OR VALUE='COMEDIA');
+create table tbl_titulo(codigo_titulo integer PRIMARY KEY, titulo text NOT NULL, categoria chk_categoria);
+
 create table tbl_emprestimo(numero_emprestimo integer PRIMARY KEY, codigo_cliente integer, codigo_livro integer);
-create table tbl_livros(codigo_livro integer PRIMARY KEY, status );
+
+CREATE DOMAIN chk_status text CHECK(VALUE='DISPONIVEL' OR VALUE='ALUGADO');
+create table tbl_livros(cod_livro integer PRIMARY KEY, codigo_titulo integer, status chk_status);
