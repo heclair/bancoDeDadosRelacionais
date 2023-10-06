b625efe1ee3b5fadef7100592089a4c2cd3b4f2b
heclair <heclair@hotmail.com>
2023-10-05 22:11:20 -0300

    aula 10

--- aula10.sql
+++ aula10.sql
@@ -191,19 +191,31 @@ Select * from v_depcidade;
 --2 crie uma view denominada v_depcidadehouston, a partir de v_depcidade que mostre somente os departamentos localizados em Houston
 -- execute select * from v_depcidadehouston
 
-CREATE OR REPLACE VIEW v_depcidadehouston AS SELECT d.nome AS nome_departamento, c.nome AS nome_cidade FROM tbl_departamentos d INNER JOIN tbl_cidades c ON d.cod_cidade = c.cod_cidade WHERE nome_departamento = 'Houston';
+CREATE OR REPLACE VIEW v_depcidadehouston AS SELECT d.* FROM tbl_departamentos d INNER JOIN tbl_cidades c ON d.cod_cidade = c.cod_cidade WHERE c.nome = 'Houston';
 
+select * from v_depcidadehouston;
 
 --3 Crie uma visão denominada de v_opsalario, a qual lista a soma e média de todos os salários dos empregados.
 -- execute select * from v_opsalario
+
+CREATE VIEW v_opsalario AS SELECT SUM(salario) AS soma_salarios, AVG(salario) AS media_salarios FROM tbl_empregados;
+
+select * from v_opsalario;
 
 
 --4 Criar a view vw_empregados_salarial que exibe o nome e o salário de todos os empregados com salário acima de 5500.
 -- execute select * from v_empregados_salarial
 
+CREATE VIEW vw_empregados_salarial AS SELECT nome, salario FROM tbl_empregados WHERE salario > 5500.00;
+
+select * from vw_empregados_salarial;
 
 --5 criar uma view v_departemp para listar os departamentos e a quantidade de empregados em cada um
 -- listar os departamentos e as quantidades por orderm decrescente de quantidade.
+
+CREATE VIEW v_departemp AS SELECT d.nome AS departamento, COUNT(e.cod_empregado) AS quantidade_empregados FROM tbl_departamentos d LEFT JOIN tbl_empregados e ON d.cod_departamento = e.cod_departamento GROUP BY d.nome, d.cod_departamento ORDER BY quantidade_empregados DESC;
+
+SELECT * FROM v_departemp;
 
 
 --6 criar uma view v_departrouble para listar os nomes dos departamentos que possuem mais projetos do que empregados.
